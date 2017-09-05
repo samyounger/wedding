dba43373e312e26158932f52aefd05fe7cdf43c9
Sam Younger <sam.younger@gmail.com>
2017-09-05 22:53:40 +0100

    google map added, plus some content

--- _includes/comments.html
+++
@@ -1,1 +0,0 @@
-<h2>comments</h2>
---
+++ _plugins/environment_variables_generator.rb
@@ -0,0 +1,13 @@
+module Jekyll
+
+  class EnvironmentVariablesGenerator < Generator
+    priority :highest
+
+    def generate(site)
+      site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
+      # Add other environment variables to `site.config` here...
+      Jekyll.logger.debug site.config.to_yaml
+    end
+  end
+
+end
