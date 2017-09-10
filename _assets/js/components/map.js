721efb056fac304283e902f3f9b0074251cce9f2
samy <sam.younger@gmail.com>
2017-09-10 15:24:22 +0100

    restyling page

--- _assets/js/components/map.js
+++ _assets/js/components/map.js
@@ -22,7 +22,7 @@ function initMap() {
   };
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    // options go here
+    scrollwheel:  false
   });
 
   bounds  = new google.maps.LatLngBounds();
